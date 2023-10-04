b7f3aa7f3e833a7756b9f89f5f59ae7feb5bd39a
Noelia Root <noeliakroot@gmail.com>
2023-10-03 21:18:17 -0600

    Initial Commit

---
+++ ConcentrationGame/ConcentrationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationGameApp.swift
+//  ConcentrationGame
+//
+//  Created by Noelia Root on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcentrationGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConcentrationGame
+//
+//  Created by Noelia Root on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
